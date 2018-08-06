de4e1c058a2e56d980d8dac06046c9265e7f4fb7
Israel Nascimento <dev.israelnascimento@gmail.com>
2018-08-06 11:21:14 -0300

    Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "react/jsx-filename-extension": 0,
-        "no-use-before-define": 0
+        "no-use-before-define": 0,
+        "max-len": ["error", 80 ]
     }
 };
