319e33ef9aaf4f79a75eff6e891a21b25b1de230
Israel Nascimento <dev.israelnascimento@gmail.com>
2018-08-26 10:06:08 -0300

    Criando actions

---
+++ src/Actions/AuthAction.js
@@ -0,0 +1,9 @@
+export const changeEmail = newEmail => ({
+  type: 'change_email',
+  payload: newEmail,
+});
+
+export const changePass = newPass => ({
+  type: 'change_pass',
+  payload: newPass,
+});
