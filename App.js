338f4ee468fddca2d70411bfd7273ad133b9b2bc
Israel Nascimento <dev.israellnascimento@gmail.com>
2019-01-19 10:50:42 -0200

    Configurando firebase

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Reducers from './src/Reducers';
 import FormLogin from './src/Screens/FormLogin';
@@ -22,8 +23,26 @@ const RootStack = createStackNavigator(
   },
 );
 
-export default () => (
-  <Provider store={createStore(Reducers)}>
-    <RootStack />
-  </Provider>
-);
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyC7fAALxtBCcebGEqiztGB1XprEL_N54l0',
+      authDomain: 'whatsapp-1dfda.firebaseapp.com',
+      databaseURL: 'https://whatsapp-1dfda.firebaseio.com',
+      projectId: 'whatsapp-1dfda',
+      storageBucket: 'whatsapp-1dfda.appspot.com',
+      messagingSenderId: '213324904258',
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(Reducers)}>
+        <RootStack />
+      </Provider>
+    );
+  }
+}
+
+export default App;
