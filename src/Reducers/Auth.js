6a239cd27cfe51fc7a44812ef4c9f34016e5e5ea
dev.israelnascimento@gmail.com <dev.israelnascimento@gmail.com>
2019-01-30 12:05:02 -0200

    mudando ifs por switch (pq nao fiz isso antes?)

--- src/Reducers/Auth.js
+++ src/Reducers/Auth.js
@@ -7,21 +7,18 @@ const INITIAL_DATA = {
 };
 
 export default (state = INITIAL_DATA, action) => {
-  console.log(action);
-  if (action.type === 'change_email') {
-    return { ...state, email: action.payload };
+  switch (action.type) {
+    case 'change_email':
+      return { ...state, email: action.payload };
+    case 'change_pass':
+      return { ...state, pass: action.payload };
+    case 'change_name':
+      return { ...state, name: action.payload };
+    case 'register_error':
+      return { ...state, errorMessageSignup: action.payload };
+    case 'login_error':
+      return { ...state, errorMessageSignin: action.payload };
+    default:
+      return state;
   }
-  if (action.type === 'change_pass') {
-    return { ...state, pass: action.payload };
-  }
-  if (action.type === 'change_name') {
-    return { ...state, name: action.payload };
-  }
-  if (action.type === 'register_error') {
-    return { ...state, errorMessageSignup: action.payload };
-  }
-  if (action.type === 'login_error') {
-    return { ...state, errorMessageSignin: action.payload };
-  }
-  return state;
 };
