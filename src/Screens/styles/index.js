7e89de8fa1b6c8e500117cfdce0e3955a6a970b8
dev.israelnascimento@gmail.com <dev.israelnascimento@gmail.com>
2019-01-30 11:46:10 -0200

    Criando arquivo com os estilos default

---
+++ src/Screens/styles/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create(
+  {
+    errorMessage: {
+      color: '#F00',
+      fontSize: 15,
+      fontWeight: 'bold',
+      padding: 5,
+    },
+  },
+);
+
+export default styles;
