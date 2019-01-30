7bded183cf1698b3d0c82edef0268982530fc8b9
dev.israelnascimento@gmail.com <dev.israelnascimento@gmail.com>
2019-01-30 21:02:45 -0200

    Colocando tela inicial como login

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const RootStack = createStackNavigator(
     Main: MainScreen,
   },
   {
-    initialRouteName: 'Main',
+    initialRouteName: 'Home',
     defaultNavigationOptions: () => ({
       headerStyle: {
         backgroundColor: '#115E54',
