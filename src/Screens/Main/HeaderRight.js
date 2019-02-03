5749b0c5cf3725d05e42470bdf091279a3aef23e
dev.israelnascimento@gmail.com <dev.israelnascimento@gmail.com>
2019-02-03 14:30:15 -0200

    Mudando Touchable

--- src/Screens/Main/HeaderRight.js
+++ src/Screens/Main/HeaderRight.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import {
-  Image, View, Text, StyleSheet, TouchableHighlight,
+  Image, View, Text, StyleSheet,
 } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { changeModalVisibilityAction } from '../../Actions/AddContactAction';
 import AddContactModal from './AddContactModal';
+import Touchable from '../Widgets/Touchable';
 
 const HeaderRight = props => (
   <View>
     <AddContactModal />
     <View style={style.viewHeader}>
-      <TouchableHighlight
-        style={style.imageHeader}
+      <Touchable
         onPress={() => props.changeModalVisibility(true)}
-        underlayColor="#114D44"
       >
         <Image
+          style={style.imageHeader}
           source={require('../../imgs/add-contact.png')}
         />
-      </TouchableHighlight>
+      </Touchable>
       <Text style={style.textHeader}>SAIR</Text>
     </View>
   </View>
