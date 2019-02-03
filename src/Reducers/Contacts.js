109c366a33ad0c77eae1e7c850c37f2e302c1513
dev.israelnascimento@gmail.com <dev.israelnascimento@gmail.com>
2019-02-03 11:40:42 -0200

    Adicionando reducer de contatos no store

--- src/Reducers/Contacts.js
+++ src/Reducers/Contacts.js
@@ -1,9 +1,13 @@
+import * as Constants from '../Constants/ContactsConst';
+
 const INITIAL_DATA = {
 
 };
 
 export default (state = INITIAL_DATA, action) => {
   switch (action.type) {
+    case Constants.CONTACT_LIST:
+      return { ...state, list: action.payload };
     default:
       return { ...state };
   }
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './Auth';
 import addContacts from './AddContacts';
+import contacts from './Contacts';
 
 export default combineReducers({
   auth,
   addContacts,
+  contacts,
 });
